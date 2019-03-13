9924278a1406b12c4266f2a7b178b10e8f8b8948
Jan200101 <sentrycraft123@gmail.com>
2019-03-13 13:58:47 +0100

    testing in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include "main.hpp"
+#include "weapons/bow.hpp"
+#include "weapons/sword.hpp"
+
+#include <iostream>
 
 int main()
 {
+    Sword* sword = new Sword;
+    while (sword->attack(true))
+    {
+    }
+
+    Bow* knife = new Bow;
+    while (knife->attack(true))
+    {
+        //std::cout << knife->getDurability() << '\n';
+    }
 }
